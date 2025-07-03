e93296ed753ec0539794f70c47856a083985e725
Krystian <krystian.wachnicki5@gmail.com>
2025-07-03 19:59:41 +0700

    feat: get title from config file

--- server/api/setup.js
+++ server/api/setup.js
@@ -1,10 +1,23 @@
-import speakeasy from "speakeasy";
+import fs from "fs";
+import path from "path";
 import qrcode from "qrcode";
+import speakeasy from "speakeasy";
+import { rootPath } from "@nstation/utils";
+import requireFromString from "require-from-string";
 
 export default async (req, res) => {
   try {
+    let config = fs.readFileSync(
+      path.join(rootPath, "nodestation.config.js"),
+      "utf-8"
+    );
+
+    const parsedConfig = requireFromString(config);
+
+    const title = parsedConfig?.site?.title || "Nodestation";
+
     const secret = speakeasy.generateSecret({
-      name: `Nodestation: ${req.user.email}`,
+      name: `${title}: ${req.user.email}`,
     });
 
     const qrCode = await qrcode.toDataURL(secret.otpauth_url);
